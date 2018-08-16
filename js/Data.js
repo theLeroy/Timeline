9e76e33e21a985701d378077b963cf3fc984f538
Leroy <4c65726f79@gmail.com>
2018-08-16 15:32:40 +0200

    added dots suport and date

--- js/Data.js
+++ js/Data.js
@@ -2,17 +2,20 @@ var Data = [
     {
     ObjectId: 1,
     Type: "Milestone",
-    StartDate: "02.12.19",
-    EndDate: "02.12.19",
+    StartDate: 1930421368800,
+    EndDate: 1930421368800,
+    LineId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    CreateChangeId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    Active: true,
     Notes: [
       {
         NoteId: 1,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 1",
         NotePerson: "Peter"
       }, {
         NoteId: 2,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 2",
         NotePerson: "Maria"
       }
@@ -23,17 +26,20 @@ var Data = [
   }, {
     ObjectId: 2,
     Type: "Milestone",
-    StartDate: "02.12.19",
-    EndDate: "02.12.19",
+    StartDate: 1930421368800,
+    EndDate: 1930421368800,
+    LineId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    CreateChangeId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    Active: true,
     Notes: [
       {
         NoteId: 1,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 1",
         NotePerson: "Peter"
       }, {
         NoteId: 2,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 2",
         NotePerson: "Maria"
       }
@@ -44,17 +50,20 @@ var Data = [
   }, {
     ObjectId: 3,
     Type: "Milestone",
-    StartDate: "02.12.19",
-    EndDate: "02.12.19",
+    StartDate: 1930421368800,
+    EndDate: 1930421368800,
+    LineId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    CreateChangeId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    Active: true,
     Notes: [
       {
         NoteId: 1,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 1",
         NotePerson: "Peter"
       }, {
         NoteId: 2,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 2",
         NotePerson: "Maria"
       }
@@ -65,17 +74,20 @@ var Data = [
   }, {
     ObjectId: 4,
     Type: "Milestone",
-    StartDate: "02.12.19",
-    EndDate: "02.12.19",
+    StartDate: 1931021068200,
+    EndDate: 1933421368800,
+    LineId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    CreateChangeId: "f8ecc80977ada358175ee64d57dc11d072beb678",
+    Active: true,
     Notes: [
       {
         NoteId: 1,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 1",
         NotePerson: "Peter"
       }, {
         NoteId: 2,
-        NoteDay: "02.12.19",
+        NoteDay: 1534421368800,
         Note: "Hay ich bin eine Notiz der Id 2",
         NotePerson: "Maria"
       }
--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,65 @@
-var c;
-var $c;
+//Variables
+var c; //Canvas
+var $c; //Type of canvas
+var DayWith; //Pixel per day
+var LineHight; //Hight of line in px
+var tempc;
+var OldestDate;
+var oldestDateIndifier;
+
+
+
+//Default Setings
+var DayWith = 50;
+var LineHight = 20;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  var c = document.getElementById("defaultCanvas0");
+  $c = c.getContext("2d");
+
+  //Generate Daylines
+  for (var i = 1; i < windowWidth/DayWith; i++) {
+    line((i*DayWith), 0, (i*DayWith), windowHeight);
+  }
+
+
+  //Get oldest date
+  OldestDate = Infinity;
+  for (var i = 0; i < Data.length; i++) {
+    if (Data[i]["StartDate"] < OldestDate) {
+      OldestDate = Data[i]["StartDate"];
+      oldestDateIndifier = i;
+    }
+  }
+
+  //Dates top
+  var ts = new Date(OldestDate);
+  for (var p = 0; p < windowWidth/DayWith; p++) {
+    // console.log(ts.toLocaleDateString());
+    textSize(10);
+    textAlign(CENTER);
+    fill(0, 102, 153);
+    text(ts.toLocaleDateString(), (p*DayWith+(DayWith/2)), 30);
+    ts = addDays(ts, 1);
+  }
+
+
+  //Dysplay dots
+  for (var i = 0; i < Data.length; i++) {
+    let d = Data[i]["StartDate"] - OldestDate;
+    d = d/1000/60/60/24;
+    ellipse((d*DayWith+(DayWith/2)), 200, 30, 30);
+    // Date[i]["EndDate"]
+  }
+
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-
 function draw() {
-   var c = document.getElementById("defaultCanvas0");
-
-   $c = c.getContext("2d");
-   background(255, 0, 200);
-   $c.beginPath();
-   $c.moveTo(20, 20);
-   $c.bezierCurveTo(20, 100, 200, 100, 200, 20);
-   $c.stroke();
 
 }
