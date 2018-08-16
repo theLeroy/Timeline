f8ecc80977ada358175ee64d57dc11d072beb678
Leroy <4c65726f79@gmail.com>
2018-08-16 10:34:19 +0200

    Add example Data

---
+++ js/Data.js
@@ -0,0 +1,87 @@
+var Data = [
+    {
+    ObjectId: 1,
+    Type: "Milestone",
+    StartDate: "02.12.19",
+    EndDate: "02.12.19",
+    Notes: [
+      {
+        NoteId: 1,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 1",
+        NotePerson: "Peter"
+      }, {
+        NoteId: 2,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 2",
+        NotePerson: "Maria"
+      }
+        ],
+    ImportantState: 1,
+    Person: ["Peter", "Sandra", "Maria"],
+    done: 1,
+  }, {
+    ObjectId: 2,
+    Type: "Milestone",
+    StartDate: "02.12.19",
+    EndDate: "02.12.19",
+    Notes: [
+      {
+        NoteId: 1,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 1",
+        NotePerson: "Peter"
+      }, {
+        NoteId: 2,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 2",
+        NotePerson: "Maria"
+      }
+        ],
+    ImportantState: 1,
+    Person: ["Peter", "Sandra", "Maria"],
+    done: 1,
+  }, {
+    ObjectId: 3,
+    Type: "Milestone",
+    StartDate: "02.12.19",
+    EndDate: "02.12.19",
+    Notes: [
+      {
+        NoteId: 1,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 1",
+        NotePerson: "Peter"
+      }, {
+        NoteId: 2,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 2",
+        NotePerson: "Maria"
+      }
+        ],
+    ImportantState: 1,
+    Person: ["Peter", "Sandra", "Maria"],
+    done: 1,
+  }, {
+    ObjectId: 4,
+    Type: "Milestone",
+    StartDate: "02.12.19",
+    EndDate: "02.12.19",
+    Notes: [
+      {
+        NoteId: 1,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 1",
+        NotePerson: "Peter"
+      }, {
+        NoteId: 2,
+        NoteDay: "02.12.19",
+        Note: "Hay ich bin eine Notiz der Id 2",
+        NotePerson: "Maria"
+      }
+        ],
+    ImportantState: 1,
+    Person: ["Peter", "Sandra", "Maria"],
+    done: 1,
+  }
+];
